efed15b4520cce3879eaf04f1d1117281f04ffe0
Dulshan Ranaweera <dulshan.ranaweera@neosolax.com>
2021-06-24 17:40:01 +0530

    fix category and product edit modell issue

--- src/app/pages/manage-categories/manage-categories.component.ts
+++ src/app/pages/manage-categories/manage-categories.component.ts
@@ -10,12 +10,12 @@ export class ManageCategoriesComponent implements OnInit {
   constructor() { }
 
   category =[
-    {categoryId:'123654',categoryName:'Herbs',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
-    {categoryId:'123654',categoryName:'Herbs',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
-    {categoryId:'123654',categoryName:'Herbs',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
-    {categoryId:'123654',categoryName:'Herbs',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
-    {categoryId:'123654',categoryName:'Herbs',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
-    {categoryId:'123654',categoryName:'Herbs',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
+    {categoryId:'123654',categoryName:'Herbs1',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
+    {categoryId:'2123654',categoryName:'Herbs2',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Inactivate'},
+    {categoryId:'3123654',categoryName:'Herbs3',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
+    {categoryId:'4123654',categoryName:'Herbs4',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
+    {categoryId:'5123654',categoryName:'Herbs5',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
+    {categoryId:'6123654',categoryName:'Herbs6',categoryThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1323/bb84496c92fd35ece3cefd6bb79bd94cac0b5d4c99eb7e172678efa41165c73ba/A',categotyStatus:'Activate'},
   ]
 
   ngOnInit(): void {
--- src/app/pages/manage-products/manage-products.component.ts
+++ src/app/pages/manage-products/manage-products.component.ts
@@ -13,11 +13,11 @@ constructor() {
 
 
   product = [
-    {productId:121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
-    {productId:121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
-    {productId:121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
-    {productId:121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
-    {productId:121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
+    {productId:1121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
+    {productId:2121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Inactive',totalQty:'200',curruntQty:'150'},
+    {productId:3121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
+    {productId:4121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
+    {productId:5121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
     {productId:121321,productName:'Cumin Powder',productThumbnail:'https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A',productCode:1454665,productPrice:'$ 9.56',productStatus:'Active',totalQty:'200',curruntQty:'150'},
     
   ]
