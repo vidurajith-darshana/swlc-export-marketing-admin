5050d2c28486917bddf30378c14c9b3e6e6f71cf
Dulshan Ranaweera <dulshan.ranaweera@neosolax.com>
2021-06-29 18:54:23 +0530

    add category service

---
+++ src/app/pages/service/admin-web-services/category.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private BASE_URL = 'http://103.125.216.56:8012/';
+
+  constructor(private httpClient : HttpClient) { }
+
+  public getAllCategory(){
+    let url = `${this.BASE_URL+'api/v1/user/category/all?page=0&size=10'}`;
+    return this.httpClient.get(url);
+  }
+}
