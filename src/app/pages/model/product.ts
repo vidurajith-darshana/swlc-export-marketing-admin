394365904dc152e4cbc8e8086fb84ea817755068
Ramesh97 <hwr@1997>
2021-08-25 23:45:30 +0530

    Version vii

--- src/app/pages/model/product.ts
+++ src/app/pages/model/product.ts
@@ -1,2 +1,15 @@
+import {Category} from './category';
+import {CategoryList} from './category-list';
+
 export class Product {
+    id : number;
+    code : string;
+    name : string;
+    thumbnail : string;
+    price : number;
+    status : string;
+    totalQty : number;
+    currentQty : number;
+    createDate : string;
+    categories : CategoryList[]
 }
