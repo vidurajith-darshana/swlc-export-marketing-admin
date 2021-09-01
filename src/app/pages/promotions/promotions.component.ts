cc03c44ff5472b1b296a0121d190bc7b6a6a5ee7
Ramesh97 <hwr@1997>
2021-09-01 20:33:28 +0530

    version promotions bugs fix

--- src/app/pages/promotions/promotions.component.ts
+++ src/app/pages/promotions/promotions.component.ts
@@ -13,6 +13,8 @@ export class PromotionsComponent implements OnInit {
 
   promotionList : Promotion[];
 
+  customSearchText : string;
+
   imageError: string;
   isImageSaved: boolean;
   cardImageBase64: string;
@@ -28,96 +30,7 @@ export class PromotionsComponent implements OnInit {
   updatePromotionHeading: string;
   updatePromotionStatus : string;
 
-  promotions = [
-    {
-      id: 1,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's not a really good product",
-      heading: "Heading",
-      created_date: '2021-07-26'
-    },
-    {
-      id: 2,
-      status: 'INACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-29'
-    },
-    {
-      id: 3,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-20'
-    },
-    {
-      id: 4,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-28'
-    },
-    {
-      id: 5,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-28'
-    },
-    {
-      id: 6,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-28'
-    },
-    {
-      id: 7,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-28'
-    },
-    {
-      id: 8,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-28'
-    },
-    {
-      id: 9,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-28'
-    },
-    {
-      id: 10,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-28'
-    },
-    {
-      id: 11,
-      status: 'ACTIVE',
-      image: "https://thumbs.nosto.com/quick/magento-f5b2bba1/8/1383/e7789469178cc18cf93ad058141fbce6adcdd7a6093ea9f12fda5c52c990b9b2a/A",
-      description: "Hi it's a really good product",
-      heading: "Heading",
-      created_date: '2021-07-28'
-    },
-  ]
+
   private options = {
     autoClose: false,
     keepAfterRouteChange: false
@@ -259,7 +172,7 @@ export class PromotionsComponent implements OnInit {
   }
 
   _getPromotionList(pageNo){
-    this.promotionService.getAllPromotions(pageNo).subscribe((data)=>{
+    this.promotionService.getAllPromotions('',pageNo).subscribe((data)=>{
       if (data['success']){
         this.promotionList = data['body'].content;
       }else{
@@ -320,6 +233,26 @@ export class PromotionsComponent implements OnInit {
     },error => {
       this.alertService.warn('Promotion delete failed!', this.options);
     })
+  }
+
+  _promotionCustomSearch(){
+    if (this.customSearchText !== '' || this.customSearchText !== undefined){
+      this.promotionService.getAllPromotions(this.customSearchText,0).subscribe((data)=>{
+        if (data['success']){
+          this.promotionList = data['body'].content;
+        }else{
+          // error
+          this.alertService.warn('Something went wrong', this.options)
+
+        }
+      },error => {
+        this.alertService.warn('Something went wrong', this.options)
+
+        // error msg
+      })
+    }else {
+      this._getPromotionList(0);
+    }
   }
 
 }
