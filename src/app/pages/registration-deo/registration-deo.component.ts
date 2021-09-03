5dd748bf70b06679d080d6f6a2c2c05380bb28b9
ObjectOne <chameera@objectoneworld.com>
2021-09-03 18:56:49 +0530

    added validation qty price total qty in add product

--- src/app/pages/registration-deo/registration-deo.component.ts
+++ src/app/pages/registration-deo/registration-deo.component.ts
@@ -12,6 +12,7 @@ import {Deo} from "../model/deo";
 })
 export class RegistrationDEOComponent implements OnInit {
     @ViewChild('closebutton') closebutton;
+    @ViewChild('closeAddDeoButton') closeAddDeoButton;
 
     categoryName = 'Angular ' + VERSION.major;
     private categoryList: Deo[];
@@ -165,8 +166,8 @@ export class RegistrationDEOComponent implements OnInit {
         this.operatorService.createOperator(operator).subscribe((data) => {
             if (data['success']) {
                 // success msg
-                this.removebackdrop();
                 this.alertService.success('DEO Added', this.options);
+                this.removebackdropAddDeo();
 
                 this._clearText();
                 this.getAllCategoryList(0);
@@ -192,5 +193,9 @@ export class RegistrationDEOComponent implements OnInit {
     }
     removebackdrop() {
         this.closebutton.nativeElement.click();
+    }
+
+    removebackdropAddDeo() {
+        this.closeAddDeoButton.nativeElement.click();
     }
 }
